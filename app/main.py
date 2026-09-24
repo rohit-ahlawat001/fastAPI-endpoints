62ef6713681967b40fa25831a3c3fc4304d9d7f8
Rohit Ahlawat <rohitahlawt160@gmail.com>
2026-09-24 22:35:39 +0530

    aded the user dummy data

--- app/main.py
+++ app/main.py
@@ -12,11 +12,16 @@ def read_root() -> dict[str, str]:
 def health_check() -> dict[str, str]:
     return {"status": "ok"}
 
-@app.get("/user")
-def getUser():
-    return{
-        "message": "Endpoint created successfuly"
-    }
+# @app.get("/user")
+# def getUser():
+#     return{
+#         "message": "Endpoint created successfuly"
+#     }
 
 # ractice Problem: User Retrieval API
 
+user_db =[
+  {"id": 1, "name": "Alice", "email": "alice@example.com"},
+  {"id": 2, "name": "Bob", "email": "bob@example.com"}
+]
+print(user_db)
